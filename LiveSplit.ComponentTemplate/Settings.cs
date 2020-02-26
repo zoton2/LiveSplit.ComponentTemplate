0b1217f86c9a03a79f74db81108b9f6c84f308b8
zoton2 <zoton2@gmail.com>
2020-02-26 01:59:18 +0000

    Add example setting

--- LiveSplit.ComponentTemplate/Settings.cs
+++ LiveSplit.ComponentTemplate/Settings.cs
@@ -5,20 +5,110 @@ namespace LiveSplit.UI.Components
 {
     public partial class Settings : UserControl
     {
+        public string ExampleSetting { get; set; }
+
+        private Label label1;
+        private TextBox exampleInput;
+        private TableLayoutPanel tableLayoutPanel1;
+
         public Settings()
         {
-            // do something
+            InitializeComponent();
+            ExampleSetting = "ExampleString";
+
+            exampleInput.DataBindings.Add("Text", this, "ExampleSetting", false, DataSourceUpdateMode.OnPropertyChanged);
         }
 
         public XmlNode GetSettings(XmlDocument document)
         {
             var parent = document.CreateElement("Settings");
+            CreateSettingsNode(document, parent);
             return parent;
+        }
+
+        private int CreateSettingsNode(XmlDocument document, XmlElement parent)
+        {
+            return SettingsHelper.CreateSetting(document, parent, "Example", ExampleSetting);
         }
 
         public void SetSettings(XmlNode settings)
         {
-            // do something
+            ExampleSetting = SettingsHelper.ParseString(settings["Example"]);
+        }
+
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.exampleInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 13.24201F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 86.75799F));
+            this.tableLayoutPanel1.Controls.Add(this.exampleInput, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(7, 7);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.91489F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 88.08511F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(430, 227);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // exampleInput
+            // 
+            this.exampleInput.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.exampleInput.Location = new System.Drawing.Point(59, 3);
+            this.exampleInput.Name = "exampleInput";
+            this.exampleInput.Size = new System.Drawing.Size(368, 20);
+            this.exampleInput.TabIndex = 2;
+            this.exampleInput.Text = "ExampleString";
+            this.exampleInput.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Example";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Settings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Settings";
+            this.Padding = new System.Windows.Forms.Padding(7);
+            this.Size = new System.Drawing.Size(444, 241);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, System.EventArgs e)
+        {
+
         }
     }
 }
